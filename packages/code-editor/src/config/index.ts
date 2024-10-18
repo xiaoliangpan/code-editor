c9b74fa24756edb0f5ee81dd9e04f47b27a826fa
panxiaoliang <panxiaoliang>
2024-10-18 13:39:11 +0800

    feat: 增加函数点击选中插件

---
+++ packages/code-editor/src/config/index.ts
@@ -0,0 +1,8 @@
+import { placeholder } from "@codemirror/view";
+
+// 一些配置项
+export const cssConfig = {
+  // css修饰函数名的className
+  funcNameClass: "cm-func-mark",
+  placeholderClass: "cm-placeholder-mark",
+};
